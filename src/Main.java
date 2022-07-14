2232bae320f1a8cf7327b86c810181076b81e852
Aman <aman.kumar99499@gmail.com>
2022-07-14 21:15:07 +0530

    Heading

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Scanner which is added to take values from the user
+        System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+      
     }
 }
 
