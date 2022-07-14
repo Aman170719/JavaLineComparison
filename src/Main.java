6e6263a621b13f3c4c009eadf6dff6c0722b177d
Aman <aman.kumar99499@gmail.com>
2022-07-14 21:16:43 +0530

    Line Comparison

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,20 @@ public class Main {
     public static void main(String[] args) {
         //Scanner which is added to take values from the user
         System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
-      
+        Scanner Scan = new Scanner(System.in);
+        //Input values from the user
+        System.out.println("Enter the Values of the line X1 and X2");
+        System.out.println("Enter the value of X1 :  ");
+        int X1 = Scan.nextInt();
+        System.out.println("Enter the value of X2 :  ");
+        int Y1 = Scan.nextInt();
+        System.out.println("Enter the Values of the line Y1 and Y2");
+        System.out.println("Enter the value of Y1 :  ");
+        int X2 = Scan.nextInt();
+        System.out.println("Enter the value of Y2 :  ");
+        int Y2 = Scan.nextInt();
+        double length = Math.sqrt((X2 - X1) * (Y2 - Y1) + (Y2 - Y1) * (Y2 - Y1));
+        System.out.println("The length of the line is  : " +length);
     }
 }
 
