bbe50da1f4e6ba08da068033696624a98861c369
Aman <106363389+Aman170719@users.noreply.github.com>
2022-08-04 09:20:38 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //Scanner which is added to take values from the user
         System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
-      
+     
     }
 }
 
